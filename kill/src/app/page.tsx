f773bf393f7282d19b7f41d85cbb1dbeaa0b97c3
Ali Ahmad <aliahmadazhar456789@gmail.com>
2025-09-04 04:14:16 -0700

    yes

--- kill/src/app/page.tsx
+++ kill/src/app/page.tsx
@@ -12,9 +12,7 @@ export default function Page() {
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale
-              on large teams. It’s easy to customize, adapts to any design,
-              and the build size is tiny.”
+              “ali.”
             </p>
           </blockquote>
           <figcaption className="font-medium">
